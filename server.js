4331ab27159efc8289b72732a0855ef5b6725ca0
ko3365 <ko3365@g.ucla.edu>
2023-05-07 16:11:55 -0700

    api endpoints added

--- server.js
+++ server.js
@@ -3,10 +3,54 @@ const fastify = Fastify({
     logger:true
 })
 
-fastify.get('/',function(request,reply){
-    reply.send('hello world')
-    //return 'hello world'
+fastify.get('/', async function(request,reply){
+    //reply.send('hello world')
+    return 'hello world'
 })
+
+// Authentication
+fastify.post('/api/auth/signup',()=>{
+    return 'signup'
+})
+
+fastify.post('/api/auth/signin',()=>{
+    return 'signin'
+})
+
+fastify.delete('/api/auth/unregister',()=>{
+    return 'unregister'
+})
+
+fastify.post('/api/auth/logout',()=>{
+    return 'logout'
+})
+
+fastify.get('/api/me',()=>{
+    return 'get me'
+})
+
+fastify.patch('/api/me/notifications',()=>{
+    return 'patch notifications'
+})
+
+// Products
+fastify.get('/api/products',()=>{
+    return 'get products'
+})
+
+fastify.post('/api/products',()=>{
+    return 'post products'
+})
+
+fastify.get('/api/products/:id',()=>{
+    return 'get single product'
+})
+
+fastify.put('/api/products/:id',()=>{
+    return 'put single product'
+})
+
+
 
 fastify.listen({port:3000},function(err,address){
     if(err){
