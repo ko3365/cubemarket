ae87476e88cda9178c35f11469d492f42c641f70
ko3365 <ko3365@g.ucla.edu>
2023-05-16 00:29:46 -0700

    tsx added

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+function main() {
+    const message: string = 'World'
+    console.log(`Hello ${message}!`)
+
+    type Test = {
+      name: string
+      age: number
+    }
+    const package:Test ={
+      name: 'hi',
+      age: 15
+    }
+  }
+  
+  main()
